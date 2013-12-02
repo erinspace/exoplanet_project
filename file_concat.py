aa2ef28dfc396cf78a978d71455e795ed2de6832
erinbspace <erin.braswell@gmail.com>
2013-12-02 18:39:46 -0500

    added xml file and python concatinator

---
+++ file_concat.py
@@ -0,0 +1,18 @@
+import shutil
+import xml.etree.ElementTree as ET, glob
+
+# create a new global xml file
+with open('systems.xml', 'wb') as outfile:
+
+    # write the opening systems tag
+    outfile.write("<systems>\n")
+
+    # loop over all of the xml files in the updated catalog
+    for filename in glob.glob('../open_exoplanet_catalogue/systems/*.xml'):
+
+    # copy the contents of that file to the master xml file
+        with open(filename) as readfile:
+            shutil.copyfileobj(readfile, outfile)
+
+    # write the closing systems tag
+    outfile.write('</systems>')
